f0ea17e0e56d5233ba4900d09e58d4df8563dbba
MarceloSazoP <marcelosazop@gmail.com>
2025-02-01 00:14:40 -0300

    first commit

--- src/app/nosotros/page.tsx
+++ src/app/nosotros/page.tsx
@@ -3,8 +3,6 @@ import { Layout1 } from "./components/Layout1";
 import { Layout2 } from "./components/Layout2";
 import { PortaFolio } from "./components/Portafolio";
 import { Team } from "./components/Team";
-import { Ubicacion } from "./components/Ubicacion";
-import { Ubicacion2 } from "./components/Ubicacion2";
 
 export default function Page() {
     return (
